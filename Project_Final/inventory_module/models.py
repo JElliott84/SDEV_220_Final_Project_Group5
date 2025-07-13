1e955b2fe3bf39c75880043478a7a8ae61e07271
Aeon <Glipman@live.com>
2025-07-12 21:15:14 -0400

    Fixing .git and venv issues

--- Project_Final/inventory_module/models.py
+++ Project_Final/inventory_module/models.py
@@ -1,18 +1,13 @@
-from django.conf import settings
 from django.db import models
-from django.utils import timezone
-
 
 class Inventory(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    # We need the material type, finish, the length, and the price per length of the coils
+    Material = models.CharField
+    (max_length=10)
+    Finish = models.CharFieldField
+    (max_length=5)
+    Length = models.IntegerfieldField
+    ()
+    Price_per_length = models.IntegerField
+    ()
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
